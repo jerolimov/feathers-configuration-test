0101f452dad23b12da40609e9e282d65005b991b
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2017-04-02 00:08:53 +0200

    Add generated feathers app with Sqlite database

--- src/services/index.js
+++ src/services/index.js
@@ -2,9 +2,18 @@
 const authentication = require('./authentication');
 const user = require('./user');
 
+const path = require('path');
+const fs = require('fs-extra');const Sequelize = require('sequelize');
 module.exports = function() {
   const app = this;
   
+  fs.ensureDirSync( path.dirname(app.get('sqlite')) );
+  const sequelize = new Sequelize('feathers', null, null, {
+    dialect: 'sqlite',
+    storage: app.get('sqlite'),
+    logging: false
+  });
+  app.set('sequelize', sequelize);
   
   app.configure(authentication);
   app.configure(user);
